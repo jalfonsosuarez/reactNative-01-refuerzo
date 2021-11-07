5d9d88c5b660cb5ad2ad5f2fe4dcc7db87bc5292
= <=>
2021-11-07 17:01:23 +0100

    Custom Hook - useCounter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 // import { TiposBasicos } from "./typescript/TiposBasicos";
 // import { ObjetosLiterales } from "./typescript/ObjetosLiterales";
 // import { Funciones } from "./typescript/Funciones";
-
-import { Contador } from "./components/Contador";
+// import { Contador } from "./components/Contador";
+import { ContadorConHook } from "./components/ContadorConHook";
 
 
 const App = () => {
@@ -13,7 +13,8 @@ const App = () => {
       {/* <TiposBasicos /> */}
       {/* <ObjetosLiterales /> */}
       {/* <Funciones /> */}
-      <Contador />
+      {/* <Contador /> */}
+      <ContadorConHook />
     </div>
   )
 }

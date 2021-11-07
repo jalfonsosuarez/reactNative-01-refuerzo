263f4fcbe85f958416ad1ff18fc70ed61586e65d
= <=>
2021-11-07 16:11:37 +0100

    01-Basic TS types

--- src/typescript/TiposBasicos.tsx
+++ src/typescript/TiposBasicos.tsx
@@ -1,8 +1,17 @@
 
 export const TiposBasicos = () => {
+
+    const nombre: string  = 'José Alfonso';
+    const edad: number = 35;
+    const estaActivo: boolean = true;
+    const poderes: Array<string> = [ 'Velocidad', 'Volar', 'Respirar en agua'];
+
     return (
         <>
             <h3>Tipos básicos.</h3>
+            { nombre }, { edad }, { estaActivo ? 'Sí' : 'No' }
+            <br />
+            { poderes.join( ', ' ) }
         </>
     )
 }
